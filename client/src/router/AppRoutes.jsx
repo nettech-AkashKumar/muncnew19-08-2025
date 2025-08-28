778ea1d3845a029908e461286e416c4c4cfbd6bc
kasimnetarioinnovations <kasimnetarioinnovations@gmail.com>
2025-08-28 18:32:04 +0530

    dfdf

--- client/src/router/AppRoutes.jsx
+++ client/src/router/AppRoutes.jsx
@@ -122,12 +122,9 @@ import ViewSupplierModal from "../pages/Modal/suppliers/ViewSupplierModal.jsx";
 import ViewProductStock from "../pages/ViewProductStock.jsx";
 import ViewReturnProduct from "../pages/ViewReturnProduct.jsx";
 import PurchaseReport from "../components/features/inventory/report/PurchaseReport.jsx";
-<<<<<<< HEAD
 import ExpenseReportProductModal from "../pages/finance&accounts/expense_report/ExpenseReportProductModal.jsx"
 
-=======
 import EditProduct from "../components/features/inventory/product/EditProduct.jsx";
->>>>>>> 2919fb8925c29d9492b34b8b18930a0c73737cb7
 
 const AppRoutes = () => {
   return (
